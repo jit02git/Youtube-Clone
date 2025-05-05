496ce76a509456786666a23594f919b3aba874b8
jitesh <jitesh02sep@gmail.com>
2025-05-05 23:15:59 +0530

    start to rebuild the youtube-clone

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const VideoContainer = () => {
   return (
     <div>
-      <h3>VideoContainer</h3>
+      <h3>this is video container to show the video</h3>
     </div>
   )
 }
