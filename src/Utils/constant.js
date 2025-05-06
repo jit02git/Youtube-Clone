15ae9ca9bf3204a67b3832b352f8e8aa85cb9ab9
jitesh <jitesh02sep@gmail.com>
2025-05-06 13:58:35 +0530

    add .env file for protecting the keys

---
+++ src/Utils/constant.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const SECRET_KEY = process.env.SECRET_KEY;
+
+export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/captions?part=snippet&videoId=M7FIvfx5J10&key=${SECRET_KEY}`;
