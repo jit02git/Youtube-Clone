3066a1702483b14647f9b72988a2056c908e0c91
jitesh <jitesh02sep@gmail.com>
2025-05-06 13:54:27 +0530

    fixed minor changes

--- src/Utils/constant.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-const SECRET_KEY = 'AIzaSyDc6ZFHnItHrZSlg9UR7YPLIAF-3T2z98U';
-
-export const YOUTUBE_VIDEOS_API = 'https://youtube.googleapis.com/youtube/v3/captions?part=snippet&videoId=M7FIvfx5J10&key='+SECRET_KEY
