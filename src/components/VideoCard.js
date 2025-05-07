60e849fd8146c29718b27f91a21097341b09f7c5
jitesh <jitesh02sep@gmail.com>
2025-05-07 22:57:09 +0530

    creating the card for the videos

---
+++ src/components/VideoCard.js
@@ -0,0 +1,7 @@
+import React from 'React'
+
+const VideoCard = () => {
+    return <div>Video Card</div>
+}
+
+export default VideoCard;
--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { YOUTUBE_VIDEOS_API } from '../Utils/constant';
 
 const VideoContainer = () => {
+
+  const [ videos, setVideos ] = useState([]);
 
   useEffect(() => {
     getVideos();
@@ -10,6 +12,9 @@ const VideoContainer = () => {
   const getVideos = async () => {
     const data =  await fetch(YOUTUBE_VIDEOS_API);
     const json = await data.json();
+    console.log("this is json data", json);
+    setVideos(json.items)
+    
   }
 
   return (
