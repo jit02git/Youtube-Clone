20dceb1d1a19585fa27962195b343cada993a162
jitesh <jitesh02sep@gmail.com>
2025-05-07 23:53:11 +0530

    working on redux

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,6 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 const WatchPage = () => {
+
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch();
+    },[])
+
   return
    (
    <div>Watch Page</div>
