5a84b18a1fe5f73fa417570c9c6bbca08b466d21
jitesh <jitesh02sep@gmail.com>
2025-05-07 23:13:10 +0530

    video card created successfully

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,7 +1,19 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
 
 const VideoCard = (info) => {
-    return <div>Video Card</div>
+    const { snippet, Statistics } = info;
+    const { channelTitle, title, thumbnail } = snippet
+    console.log("showing info from video card file", info)
+    return
+    <div>
+            <img alt='video' src={thumbnail.medium.url}/>
+            <ul>
+                <li>{title}</li>
+                <li>{channelTitle}</li>
+                <li>{Statistics.viewCount}</li>
+            </ul>
+    </div>
 }
 
 export default VideoCard;
